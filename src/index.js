c3d159899b6e77d7436545063630b78f0f183a5a
Gad-Iradufasha <legmeekmc4@gmail.com>
2025-03-05 23:00:28 +0200

    Project setup commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/first', (req, res) => {
+    res.send('First Page');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
